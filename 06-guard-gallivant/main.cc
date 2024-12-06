9a48eff1f34a71e73d150f2c74f4053f494d67b6
David Jonsson <david.jonsson306@gmail.com>
2024-12-06 09:56:27 +0100

    [day6] Refactor structs

--- 06-guard-gallivant/main.cc
+++ 06-guard-gallivant/main.cc
@@ -4,12 +4,12 @@
 #include <numeric>
 #include <string>
 #include <unordered_set>
-#include <utility>
 #include <vector>
 
 #include "2dgrid.h"
 
 using position = std::complex<int64_t>;
+using history  = std::unordered_set<position>;
 
 struct visit {
     position where, direction;
@@ -28,17 +28,27 @@ struct std::hash<visit> {
     }
 };
 
-struct stuck {
+struct lab {
     int                       direction = 0;
-    position                  where, p;
+    position                  where;
     std::vector<position>     paths{ { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
     std::unordered_set<visit> visited;
     std::vector<std::string>  grid;
 
-    stuck(std::vector<std::string> g, position start, position extra) : where(start), p(extra), grid(g) {
-        visited.insert({ where, paths[direction] });
-        set(p, '#');
+    lab(std::istream& is) {
+        std::string s;
+        for (int i = 0; std::getline(is, s); ++i) {
+            for (int j = 0; j < s.size(); ++j) {
+                if (s[j] == '^') {
+                    where = { i, j };
+                    visited.insert({ where, paths[direction] });
+                }
+            }
+            grid.push_back(s);
+        }
     }
+
+    lab(position w, std::vector<std::string> g) : where(w), grid(g) { visited.insert({ where, paths[direction] }); }
 
     bool valid(position p) {
         return p.real() >= 0 and p.real() < grid.back().size() and p.imag() >= 0 and p.imag() < grid.size();
@@ -62,73 +72,53 @@ struct stuck {
         return true;
     }
 
-    bool is_stuck() {
-        if (p == where) return false;
+    void traverse() {
         while (valid(where)) {
             while (peek() == '#') { update_direction(); }
             if (!walk()) break;
         }
+    }
+};
+
+struct stuck : public lab {
+    position block;
+
+    stuck(std::vector<std::string> g, position start, position b) : lab(start, g), block(b) { set(block, '#'); }
+
+    bool is_stuck() {
+        if (block == where) return false;
+        traverse();
         return valid(where);
     }
 };
 
-struct lab {
-    int                          direction = 0;
-    position                     where, start;
-    std::unordered_set<position> visited;
-    std::vector<position>        paths{ { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-    std::vector<std::string>     grid;
+struct guard : lab {
+    position start;
+    history  uniq;
 
-    lab(std::istream& is) {
-        std::string s;
-        for (int i = 0; std::getline(is, s); ++i) {
-            for (int j = 0; j < s.size(); ++j) {
-                if (s[j] == '^') {
-                    where = { i, j };
-                    start = where;
-                    s[j]  = '.';
-                    visited.insert(where);
-                }
-            }
-            grid.push_back(s);
-        }
-    }
-
-    bool valid(position p) {
-        return p.real() >= 0 and p.real() < grid.back().size() and p.imag() >= 0 and p.imag() < grid.size();
-    }
-
-    char get(position p) { return valid(p) ? grid[p.real()][p.imag()] : '-'; }
-
-    void update_direction() { direction = (direction + 1) % paths.size(); }
-
-    char peek() { return get(where + paths[direction]); }
-
-    void walk() {
-        where += paths[direction];
-        if (valid(where)) { visited.insert(where); }
-    }
+    guard(std::istream& is) : lab(is), start(where) {}
 
     int part1() {
-        while (valid(where)) {
-            while (peek() == '#') { update_direction(); }
-            walk();
-        }
-        return visited.size();
+        traverse();
+        uniq = std::accumulate(visited.begin(), visited.end(), history{}, [](history acc, visit v) {
+            acc.insert(v.where);
+            return acc;
+        });
+        return uniq.size();
     }
 
     int part2() const {
-        std::vector<stuck> extra = std::accumulate(
-                visited.begin(), visited.end(), std::vector<stuck>{}, [this](std::vector<stuck> acc, position p) {
-                    acc.emplace_back(grid, start, p);
+        std::vector<stuck> ss = std::accumulate(
+                uniq.begin(), uniq.end(), std::vector<stuck>{}, [this](std::vector<stuck> acc, position block) {
+                    acc.emplace_back(grid, start, block);
                     return acc;
                 });
-        return std::count_if(std::execution::par, extra.begin(), extra.end(), [](stuck s) { return s.is_stuck(); });
+        return std::count_if(std::execution::par, ss.begin(), ss.end(), [](stuck s) { return s.is_stuck(); });
     }
 };
 
 int main(int argc, char* argv[]) {
-    lab l(std::cin);
+    guard l(std::cin);
     std::cout << "Part1: " << l.part1() << "\n";
     std::cout << "Part2: " << l.part2() << "\n";
     return 0;
