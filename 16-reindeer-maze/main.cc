a6ca2e2e221167b8054a06f28a581489dbe7c18e
David Jonsson <david.jonsson306@gmail.com>
2024-12-19 19:36:12 +0100

    [day16] Cleanup of day16

--- 16-reindeer-maze/main.cc
+++ 16-reindeer-maze/main.cc
@@ -1,9 +1,13 @@
 #include <algorithm>
 #include <complex>
+#include <cstddef>
 #include <deque>
+#include <functional>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <print>
+#include <queue>
 #include <string>
 #include <tuple>
 #include <unordered_map>
@@ -13,12 +17,27 @@
 
 using namespace std;
 
-using pos = complex<int>;
+using pos     = complex<int>;
+using queue_t = tuple<size_t, pos, pos>;
+struct queue_less {
+    bool operator()(const queue_t &lhs, const queue_t &rhs) { return get<0>(lhs) > get<0>(rhs); }
+};
+using set_t     = pair<pos, pos>;
+using visited_t = pair<set_t, size_t>;
+using pq        = priority_queue<queue_t, deque<queue_t>, queue_less>;
 
 template <>
 struct std::hash<pos> {
     std::size_t operator()(const pos &p) const {
         return std::hash<int64_t>()(p.real()) ^ (std::hash<int64_t>()(p.imag()) << 1);
+    }
+};
+
+template <>
+struct std::hash<set_t> {
+    std::size_t operator()(const set_t &s) const {
+        return std::hash<int64_t>()(s.first.real()) ^ (std::hash<int64_t>()(s.first.imag()) << 1) ^
+               (std::hash<int64_t>()(s.second.imag()) << 2) ^ (std::hash<int64_t>()(s.second.imag()) << 3);
     }
 };
 
@@ -28,18 +47,17 @@ auto pop(auto &q) {
     return p;
 }
 
-using step = tuple<pos, int, int, unordered_set<pos>>;
-
 struct maze {
-    vector<string> grid;
-    pos            start, end;
-    vector<pos>    dirs{ { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+    vector<string>               grid;
+    pos                          start, end;
+    unordered_map<set_t, size_t> visited;
 
     maze(istream &is) {
         string s;
         while (getline(is, s)) { grid.push_back(s); }
         start = locate('S');
         end   = locate('E');
+        traverse();
     }
 
     char get(pos at) { return grid[at.imag()][at.real()]; }
@@ -52,73 +70,59 @@ struct maze {
         return { 0, 0 };
     }
 
-    int turn_left(int dir) {
-        dir--;
-        if (dir < 0) dir += dirs.size();
-        return dir;
-    }
+    void traverse() {
+        pq q;
+        q.push({ 0, start, pos{ 1, 0 } });
+        for (; !q.empty(); q.pop()) {
+            auto [score, current, dir] = q.top();
 
-    int turn_right(int dir) {
-        dir++;
-        return dir % dirs.size();
-    }
+            set_t key = { current, dir };
+            if (visited.contains(key) and visited[key] < score) { continue; }
+            visited[key] = score;
 
-    int part1() {
-        unordered_map<pos, int> visited;
-
-        deque<step> paths{ { start, 0, 0, {} } };
-        while (paths.size()) {
-            auto [current, dir, score, history] = pop(paths);
-            if (get(current) == '#') continue;
-            if (visited.contains(current) and visited[current] <= score) { continue; }
-            visited[current] = score;
-
-            int left = turn_left(dir), right = turn_right(dir);
-            paths.push_back({ current + dirs[left], left, score + 1001, history });
-            paths.push_back({ current + dirs[dir], dir, score + 1, history });
-            paths.push_back({ current + dirs[right], right, score + 1001, history });
+            for (auto qt : vector<queue_t>{ { score + 1, current + dir, dir },
+                                            { score + 1000, current, pos{ -dir.imag(), dir.real() } },
+                                            { score + 1000, current, pos{ dir.imag(), -dir.real() } } }) {
+                auto [nscore, ncurrent, ndir] = qt;
+                if (get(ncurrent) == '#') continue;
+                q.push(qt);
+            }
         }
-
-        return visited[end];
     }
 
-    // 7036
-    // 11048
-    // 101492
+    size_t trace_back(unordered_set<set_t> target) {
+        unordered_set<pos> seen;
+        deque<set_t>       q{ target.begin(), target.end() };
+        while (q.size()) {
+            set_t key = pop(q);
+
+            const auto [current, dir] = key;
+            seen.insert(current);
+            vector<tuple<set_t, size_t>> newkeys{ { { current - dir, dir }, 1 },
+                                                  { { current, pos{ -dir.imag(), dir.real() } }, 1000 },
+                                                  { { current, pos{ dir.imag(), -dir.real() } }, 1000 } };
+            for (auto const [newkey, score] : newkeys) {
+                if (visited.contains(newkey) and visited[newkey] + score == visited[key]) q.push_back(newkey);
+            }
+        }
+        return seen.size();
+    }
+
+    size_t part1() {
+        size_t m = numeric_limits<size_t>::max();
+        for (visited_t vt : visited) {
+            if (vt.first.first == end) { m = min(m, vt.second); }
+        }
+        return m;
+    }
+
     int part2() {
-        unordered_map<pos, int> visited;
-        unordered_set<pos>      best;
-
-        deque<step> paths{ { start, 0, 0, {} } };
-        while (paths.size()) {
-            auto [current, dir, score, history] = pop(paths);
-            if (get(current) == '#') continue;
-            if (visited.contains(current) and visited[current] * 2 < score) { continue; }
-            if (!visited.contains(current)) {
-                visited[current] = score;
-            } else if (score < visited[current]) {
-                visited[current] = score;
-            }
-            history.insert(current);
-            if (current == end and score == 101492) {
-                copy(history.begin(), history.end(), inserter(best, best.begin()));
-            }
-
-            int left = turn_left(dir), right = turn_right(dir);
-            paths.push_back({ current + dirs[left], left, score + 1001, history });
-            paths.push_back({ current + dirs[dir], dir, score + 1, history });
-            paths.push_back({ current + dirs[right], right, score + 1001, history });
+        size_t               target = part1();
+        unordered_set<set_t> from;
+        for (visited_t vt : visited) {
+            if (vt.first.first == end and vt.second == target) { from.insert(vt.first); }
         }
-
-        print_grid(best);
-
-        return best.size();
-    }
-
-    void print_grid(unordered_set<pos> paths) {
-        auto cp = grid;
-        for (auto p : paths) { cp[p.imag()][p.real()] = 'O'; }
-        copy(cp.begin(), cp.end(), ostream_iterator<string>(cout, "\n"));
+        return trace_back(from);
     }
 };
 
