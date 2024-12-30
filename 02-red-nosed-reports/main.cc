c4980c5a49c0a76661c1b835349504989775246e
David Jonsson <david.jonsson306@gmail.com>
2024-12-30 13:31:51 +0100

    [day2] Revisited day2

--- 02-red-nosed-reports/main.cc
+++ 02-red-nosed-reports/main.cc
@@ -1,28 +1,31 @@
 #include <algorithm>
 #include <cstdlib>
+#include <functional>
 #include <iostream>
+#include <ranges>
 #include <string>
+#include <tuple>
 #include <vector>
 
-#include "split.h"
+#include "util.h"
 
-using report  = std::vector<int>;
-using reports = std::vector<report>;
+using namespace std;
 
-bool is_sorted(report r) {
-    return std::is_sorted(r.begin(), r.end()) ||
-           std::is_sorted(r.begin(), r.end(), [](int lhs, int rhs) { return lhs > rhs; });
-}
+using report  = vector<int>;
+using reports = vector<report>;
+
+bool is_sorted(report r) { return is_sorted(r.begin(), r.end()) or is_sorted(r.begin(), r.end(), greater<int>()); }
 
 bool element_difference_check(report r) {
-    for (int i = 0; i < r.size() - 1; i++) {
-        int value = std::abs(r[i] - r[i + 1]);
-        if (value > 3 || value == 0) { return false; }
-    }
-    return true;
+    auto view = r | std::views::adjacent<2>;
+    auto it   = ranges::find_if(view, [](tuple<int, int> p) {
+        int value = abs(get<0>(p) - get<1>(p));
+        return (value > 3 or value == 0);
+    });
+    return it == view.end();
 }
 
-bool part1_predicate(report r) { return is_sorted(r) && element_difference_check(r); }
+bool part1_predicate(report r) { return is_sorted(r) and element_difference_check(r); }
 
 bool part2_predicate(report r) {
     bool safe = part1_predicate(r);
@@ -37,15 +40,15 @@ bool part2_predicate(report r) {
 }
 
 reports parse() {
-    reports     rs;
-    std::string s;
-    while (std::getline(std::cin, s)) rs.push_back(split<int>(s));
+    reports rs;
+    string  s;
+    while (getline(cin, s)) rs.push_back(split<int>(s));
     return rs;
 }
 
 int main(int argc, char *argv[]) {
     reports rs = parse();
-    std::cout << "Part1: " << std::count_if(rs.begin(), rs.end(), part1_predicate) << "\n";
-    std::cout << "Part2: " << std::count_if(rs.begin(), rs.end(), part2_predicate) << "\n";
+    cout << "Part1: " << count_if(rs.begin(), rs.end(), part1_predicate) << "\n";
+    cout << "Part2: " << count_if(rs.begin(), rs.end(), part2_predicate) << "\n";
     return 0;
 }
