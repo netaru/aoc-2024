352b30c52399de5414edfa0142a11f3b686500cc
David Jonsson <david.jonsson306@gmail.com>
2025-01-02 21:56:34 +0100

    [day14] touchup day14

--- 14-restroom-redoubt/main.cc
+++ 14-restroom-redoubt/main.cc
@@ -5,8 +5,10 @@
 #include <iterator>
 #include <limits>
 #include <numeric>
+#include <optional>
 #include <print>
 #include <span>
+#include <sstream>
 #include <string>
 #include <vector>
 #include "split.h"
@@ -38,19 +40,19 @@ struct robot {
         if (y >= y_size) { y %= y_size; }
     }
 
-    pair<int, bool> quadrant() {
-        if (x >= 0 and x < half_x and y >= 0 and y < half_y) return { 0, true };
-        if (x > half_x and x < x_size and y >= 0 and y < half_y) return { 1, true };
-        if (x >= 0 and x < half_x and y > half_y and y < y_size) return { 2, true };
-        if (x >= 0 and x > half_x and y > half_y and y < y_size) return { 3, true };
-        return { 0, false };
+    optional<int> quadrant() const {
+        if (x >= 0 and x < half_x and y >= 0 and y < half_y) return 0;
+        if (x > half_x and x < x_size and y >= 0 and y < half_y) return 1;
+        if (x >= 0 and x < half_x and y > half_y and y < y_size) return 2;
+        if (x >= 0 and x > half_x and y > half_y and y < y_size) return 3;
+        return {};
     }
 };
 
-int product(const vector<robot> robots) {
+int product(span<robot> robots) {
     array<int, 4> q{ 0, 0, 0, 0 };
-    for (auto r : robots) {
-        if (auto [quad, valid] = r.quadrant(); valid) { q[quad]++; }
+    for (const auto& r : robots) {
+        if (auto quad = r.quadrant(); quad.has_value()) { q[quad.value()]++; }
     }
     return accumulate(q.begin(), q.end(), 1, multiplies());
 }
@@ -67,28 +69,30 @@ vector<robot> parse(istream& is) {
     return robots;
 }
 
-void print_grid(span<robot> robots) {
-    vector<string> grid(y_size);
-    for (auto& s : grid) { s = string(x_size, '.'); }
+string build_grid(span<robot> robots) {
+    vector<string> grid(y_size, string(x_size, '.'));
     for (const auto r : robots) { grid[r.y][r.x] = '#'; }
-    copy(grid.begin(), grid.end(), ostream_iterator<string>(cout, "\n"));
+    ostringstream oss;
+    copy(grid.begin(), grid.end(), ostream_iterator<string>(oss, "\n"));
+    return oss.str();
 }
 
 int main(int argc, char* argv[]) {
-    int  part1, min_product = numeric_limits<int>::max(), min_iteration = 0;
-    auto robots = parse(cin);
+    int    part1, min_product = numeric_limits<int>::max(), min_iteration = 0;
+    auto   robots = parse(cin);
+    string last;
     for (int i = 1; i < 10000; ++i) {
         for_each(robots.begin(), robots.end(), [](robot& r) { r.tick(); });
         int current = product(robots);
         if (current < min_product) {
             min_product   = current;
             min_iteration = i;
-            print("New min product at iteration {}\n", i);
-            print_grid(robots);
+            last          = build_grid(robots);
         }
         if (i == 100) { part1 = current; }
     }
     print("Part1: {}\n", part1);
     print("Part2: {}\n", min_iteration);
+    print("{}\n", last);
     return 0;
 }
