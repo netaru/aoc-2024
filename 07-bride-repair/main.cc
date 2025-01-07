4898e667c0905c2bf605bae15c2fce72cfaacc29
David Jonsson <david.jonsson306@gmail.com>
2025-01-07 12:57:43 +0100

    [day07] Day 7 touchup

--- 07-bride-repair/main.cc
+++ 07-bride-repair/main.cc
@@ -4,34 +4,29 @@
 #include <iostream>
 #include <istream>
 #include <numeric>
+#include <ostream>
+#include <ranges>
 #include <string>
 #include <vector>
 
-#include "split.h"
+#include "util.h"
 
-using input_t  = std::vector<std::string>;
-using int_t    = std::uint64_t;
-using values_t = std::vector<int_t>;
+using namespace std;
 
 struct problem {
-    input_t  input;
-    int_t    target;
-    values_t values;
+    vector<string>  input;
+    int64_t         target;
+    vector<int64_t> values;
 
-    constexpr size_t digits(const int_t value) const { return std::log10(value) + 1; }
-    constexpr int_t  concat(const int_t sum, const int_t value) const {
-        return sum * std::powl(10, digits(value)) + value;
-    }
+    constexpr size_t  digits(int64_t value) const { return log10(value) + 1; }
+    constexpr int64_t concat(int64_t sum, int64_t value) const { return sum * powl(10, digits(value)) + value; }
 
     template <int part>
-    bool solveable(values_t::const_iterator iter, const int_t sum) const {
+    bool solveable(auto iter, int64_t sum) const {
         if (iter == values.cend() or sum > target) return sum == target;
-        if constexpr (part == 1) {
-            return solveable<part>(iter + 1, sum + (*iter)) or solveable<part>(iter + 1, sum * (*iter));
-        } else {
-            return solveable<part>(iter + 1, sum + (*iter)) or solveable<part>(iter + 1, sum * (*iter)) or
-                   solveable<part>(iter + 1, concat(sum, (*iter)));
-        }
+        bool result = solveable<part>(iter + 1, sum + (*iter)) or solveable<part>(iter + 1, sum * (*iter));
+        if constexpr (part == 2) { result |= solveable<part>(iter + 1, concat(sum, (*iter))); }
+        return result;
     }
 
     template <int part = 1>
@@ -39,35 +34,14 @@ struct problem {
         return solveable<part>(values.cbegin() + 1, *values.cbegin());
     }
 
-    problem(std::string s)
-        : input(split(s, ':')), target(std::stoull(input.front())), values(split<int_t>(input.back())) {}
-};
-
-struct calc {
-    std::vector<problem> problems;
-
-    calc(std::istream &is) {
-        std::string s;
-        while (std::getline(is, s)) { problems.push_back({ s }); }
-    }
-
-    int_t part1() {
-        return std::accumulate(problems.cbegin(), problems.cend(), int_t(0), [](int_t acc, problem p) {
-            if (p.solveable()) { acc += p.target; }
-            return acc;
-        });
-    }
-
-    int_t part2() {
-        return std::accumulate(problems.cbegin(), problems.cend(), 0ull, [](int_t acc, problem p) {
-            return acc + (p.solveable<2>() ? p.target : 0);
-        });
-    }
+    problem(string s) : input(split(s, ":")), target(stoll(input.front())), values(split<int64_t>(input.back())) {}
 };
 
 int main(int argc, char *argv[]) {
-    calc calculator(std::cin);
-    std::cout << "Part1: " << calculator.part1() << "\n";
-    std::cout << "Part2: " << calculator.part2() << "\n";
+    auto ps  = split(read(cin), "\n") | views::transform([](const auto &s) { return problem{ s }; });
+    auto fn1 = [](problem p) { return p.solveable<1>() ? p.target : 0; };
+    auto fn2 = [](problem p) { return p.solveable<2>() ? p.target : 0; };
+    println("Part1: {}", transform_reduce(ps.begin(), ps.end(), 0l, plus(), fn1));
+    println("Part2: {}", transform_reduce(ps.begin(), ps.end(), 0l, plus(), fn2));
     return 0;
 }
