3d55a4f2026a1a63e1a03c42cbef83c9fa424c45
David Jonsson <david.jonsson306@gmail.com>
2025-01-07 10:39:16 +0100

    [day05] Removed the class

--- 05-print-queue/main.cc
+++ 05-print-queue/main.cc
@@ -11,32 +11,17 @@
 using namespace std;
 constexpr int middle(const auto &p) { return p[(p.size() / 2)]; }
 
-struct printer {
-    map<int, set<int>>       rules;
-    vector<vector<int>>      pages;
-    function<bool(int, int)> comp;
-
-    printer(istream &is) : comp([this](int lhs, int rhs) { return rules[lhs].contains(rhs); }) {
-        auto input = split(read(is), "\n\n");
-        for (const auto &rule : split(input[0], "\n") | views::transform([](auto s) { return split<int>(s, "|"); }))
-            rules[rule.front()].insert(rule.back());
-        for (const auto &s : split(input[1], "\n")) { pages.push_back(split<int>(s, ",")); }
-    }
-
-    template <int part>
-    int solve() {
-        function<int(span<int>)> fn;
-        if constexpr (part == 1)
-            fn = [&](auto p) { return ranges::is_sorted(p, comp) ? middle(p) : 0; };
-        else
-            fn = [&](auto p) { return ranges::is_sorted(p, comp) ? 0 : middle(dave::sort(p, comp)); };
-        return transform_reduce(pages.begin(), pages.end(), 0, plus(), fn);
-    }
-};
-
 int main(int argc, char *argv[]) {
-    printer p(cin);
-    cout << "Part1: " << p.solve<1>() << "\n";
-    cout << "Part2: " << p.solve<2>() << "\n";
+    map<int, set<int>>  rules;
+    vector<vector<int>> pages;
+    auto                comp{ [&rules](int lhs, int rhs) { return rules[lhs].contains(rhs); } };
+    auto                input = split(read(cin), "\n\n");
+    for (const auto &rule : split(input[0], "\n") | views::transform([](auto s) { return split<int>(s, "|"); }))
+        rules[rule.front()].insert(rule.back());
+    for (const auto &s : split(input[1], "\n")) { pages.push_back(split<int>(s, ",")); }
+    auto fn1 = [&](auto p) { return ranges::is_sorted(p, comp) ? middle(p) : 0; };
+    auto fn2 = [&](auto p) { return ranges::is_sorted(p, comp) ? 0 : middle(dave::sort(p, comp)); };
+    println("Part1: {}", transform_reduce(pages.begin(), pages.end(), 0, plus(), fn1));
+    println("Part2: {}", transform_reduce(pages.begin(), pages.end(), 0, plus(), fn2));
     return 0;
 }
