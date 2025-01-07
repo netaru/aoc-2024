21e67b577b647bead7d7143f219ccd1ecf0331e0
David Jonsson <david.jonsson306@gmail.com>
2025-01-07 13:08:17 +0100

    [day07] Removed classes

--- 07-bride-repair/main.cc
+++ 07-bride-repair/main.cc
@@ -1,47 +1,43 @@
 #include <cmath>
-#include <cstddef>
-#include <cstdint>
 #include <iostream>
-#include <istream>
 #include <numeric>
-#include <ostream>
 #include <ranges>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "util.h"
 
 using namespace std;
 
-struct problem {
-    vector<string>  input;
-    int64_t         target;
-    vector<int64_t> values;
+constexpr int64_t digits(int64_t value) { return log10(value) + 1; }
+constexpr int64_t concat(int64_t sum, int64_t value) { return sum * powl(10, digits(value)) + value; }
 
-    constexpr size_t  digits(int64_t value) const { return log10(value) + 1; }
-    constexpr int64_t concat(int64_t sum, int64_t value) const { return sum * powl(10, digits(value)) + value; }
+template <int part>
+bool solveable(auto iter, auto end, int64_t sum, int64_t target) {
+    if (iter == end or sum > target) return sum == target;
+    bool result = solveable<part>(iter + 1, end, sum + (*iter), target) or
+                  solveable<part>(iter + 1, end, sum * (*iter), target);
+    if constexpr (part == 2) { result |= solveable<part>(iter + 1, end, concat(sum, (*iter)), target); }
+    return result;
+}
 
-    template <int part>
-    bool solveable(auto iter, int64_t sum) const {
-        if (iter == values.cend() or sum > target) return sum == target;
-        bool result = solveable<part>(iter + 1, sum + (*iter)) or solveable<part>(iter + 1, sum * (*iter));
-        if constexpr (part == 2) { result |= solveable<part>(iter + 1, concat(sum, (*iter))); }
-        return result;
-    }
+template <int part>
+bool solveable(vector<int64_t> values, int64_t target) {
+    return solveable<part>(values.cbegin() + 1, values.cend(), *values.cbegin(), target);
+}
 
-    template <int part = 1>
-    bool solveable() const {
-        return solveable<part>(values.cbegin() + 1, *values.cbegin());
-    }
-
-    problem(string s) : input(split(s, ":")), target(stoll(input.front())), values(split<int64_t>(input.back())) {}
-};
+template <int part>
+int64_t solve(pair<vector<int64_t>, int64_t> p) {
+    return solveable<part>(p.first, p.second) ? p.second : 0;
+}
 
 int main(int argc, char *argv[]) {
-    auto ps  = split(read(cin), "\n") | views::transform([](const auto &s) { return problem{ s }; });
-    auto fn1 = [](problem p) { return p.solveable<1>() ? p.target : 0; };
-    auto fn2 = [](problem p) { return p.solveable<2>() ? p.target : 0; };
-    println("Part1: {}", transform_reduce(ps.begin(), ps.end(), 0l, plus(), fn1));
-    println("Part2: {}", transform_reduce(ps.begin(), ps.end(), 0l, plus(), fn2));
+    auto ps = split(read(cin), "\n") | views::transform([](const auto &s) {
+                  auto input = split(s, ":");
+                  return make_pair(split<int64_t>(input.back()), stoll(input.front()));
+              });
+    println("Part1: {}", transform_reduce(ps.begin(), ps.end(), 0l, plus(), solve<1>));
+    println("Part2: {}", transform_reduce(ps.begin(), ps.end(), 0l, plus(), solve<2>));
     return 0;
 }
