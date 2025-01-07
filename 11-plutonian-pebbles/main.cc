3efe3cfa35c893981d12e748c076143205ea9559
David Jonsson <david.jonsson306@gmail.com>
2025-01-07 17:54:43 +0100

    [day11] Touchup

--- 11-plutonian-pebbles/main.cc
+++ 11-plutonian-pebbles/main.cc
@@ -8,19 +8,20 @@
 #include <utility>
 #include <vector>
 
+#include "util.h"
+
 using namespace std;
 
-using int_t       = int64_t;
-using cache_key_t = pair<int_t, int_t>;
-using cache_t     = map<cache_key_t, int_t>;
+using cache_key_t = pair<i64, i64>;
+using cache_t     = map<cache_key_t, i64>;
 
 cache_t cache;
-int_t   solve(int_t value, int_t blinks) {
+i64     solve(i64 value, i64 blinks) {
     cache_key_t key{ value, blinks };
     if (blinks == 0) return 1;
     if (cache.contains(key)) return cache[key];
 
-    int_t result(0);
+    i64 result(0);
     if (value == 0) {
         result = solve(1, blinks - 1);
     } else if (string s = to_string(value); s.size() % 2 == 0) {
@@ -32,13 +33,14 @@ int_t   solve(int_t value, int_t blinks) {
     return result;
 }
 
-auto fn(const int_t steps) {
-    return [steps](int_t acc, int_t value) { return acc + solve(value, steps); };
+template <int steps>
+auto fn() {
+    return [](i64 value) { return solve(value, steps); };
 }
 
 int main(int argc, char* argv[]) {
-    vector<int_t> values{ istream_iterator<int_t>{ cin }, {} };
-    print("Part 1: {}\n", accumulate(values.begin(), values.end(), 0ull, fn(25)));
-    print("Part 2: {}\n", accumulate(values.begin(), values.end(), 0ull, fn(75)));
+    vector<i64> xs{ istream_iterator<i64>{ cin }, {} };
+    print("Part 1: {}\n", transform_reduce(xs.begin(), xs.end(), 0ll, plus(), fn<25>()));
+    print("Part 2: {}\n", transform_reduce(xs.begin(), xs.end(), 0ll, plus(), fn<75>()));
     return 0;
 }
