1d58295249fac760be82b853655259c49bf4a445
David Jonsson <david.jonsson306@gmail.com>
2025-01-10 22:18:57 +0100

    [day16] Some improvements

--- 16-reindeer-maze/main.cc
+++ 16-reindeer-maze/main.cc
@@ -2,105 +2,64 @@
 #include <complex>
 #include <cstddef>
 #include <deque>
-#include <functional>
 #include <iostream>
-#include <iterator>
 #include <limits>
 #include <print>
 #include <queue>
-#include <string>
 #include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
+#include "util.h"
 
 using namespace std;
 
-using pos     = complex<int>;
-using queue_t = tuple<size_t, pos, pos>;
+using queue_t = tuple<pair<pos, pos>, size_t>;
 struct queue_less {
-    bool operator()(const queue_t &lhs, const queue_t &rhs) { return get<0>(lhs) > get<0>(rhs); }
+    bool operator()(const queue_t &lhs, const queue_t &rhs) { return get<1>(lhs) > get<1>(rhs); }
 };
 using set_t     = pair<pos, pos>;
-using visited_t = pair<set_t, size_t>;
+using visited_t = unordered_map<set_t, size_t>;
 using pq        = priority_queue<queue_t, deque<queue_t>, queue_less>;
 
-template <>
-struct std::hash<pos> {
-    std::size_t operator()(const pos &p) const {
-        return std::hash<int64_t>()(p.real()) ^ (std::hash<int64_t>()(p.imag()) << 1);
-    }
-};
-
-template <>
-struct std::hash<set_t> {
-    std::size_t operator()(const set_t &s) const {
-        return std::hash<int64_t>()(s.first.real()) ^ (std::hash<int64_t>()(s.first.imag()) << 1) ^
-               (std::hash<int64_t>()(s.second.imag()) << 2) ^ (std::hash<int64_t>()(s.second.imag()) << 3);
-    }
-};
-
-auto pop(auto &q) {
-    auto p = q.front();
-    q.pop_front();
-    return p;
-}
-
 struct maze {
-    vector<string>               grid;
-    pos                          start, end;
-    unordered_map<set_t, size_t> visited;
+    plane     pl;
+    pos       start, end;
+    visited_t visited;
 
-    maze(istream &is) {
-        string s;
-        while (getline(is, s)) { grid.push_back(s); }
-        start = locate('S');
-        end   = locate('E');
-        traverse();
-    }
-
-    char get(pos at) { return grid[at.imag()][at.real()]; }
-    pos  locate(char ch) {
-        for (int y = 0; y < grid.size(); ++y) {
-            for (int x = 0; x < grid[0].size(); ++x) {
-                if (grid[y][x] == ch) return { x, y };
-            }
-        }
-        return { 0, 0 };
-    }
+    maze(istream &is) : pl(is), start(*pl.find('S').begin()), end(*pl.find('E').begin()) { traverse(); }
 
     void traverse() {
         pq q;
-        q.push({ 0, start, pos{ 1, 0 } });
+        q.push({ { start, pos{ 1, 0 } }, 0 });
         for (; !q.empty(); q.pop()) {
-            auto [score, current, dir] = q.top();
-
-            set_t key = { current, dir };
+            auto [key, score]   = q.top();
+            auto [current, dir] = key;
             if (visited.contains(key) and visited[key] < score) { continue; }
             visited[key] = score;
 
-            for (auto qt : vector<queue_t>{ { score + 1, current + dir, dir },
-                                            { score + 1000, current, pos{ -dir.imag(), dir.real() } },
-                                            { score + 1000, current, pos{ dir.imag(), -dir.real() } } }) {
-                auto [nscore, ncurrent, ndir] = qt;
-                if (get(ncurrent) == '#') continue;
+            for (auto qt : vector<queue_t>{ { { current + dir, dir }, score + 1 },
+                                            { { current, clockwise(dir) }, score + 1000 },
+                                            { { current, cclockwise(dir) }, score + 1000 } }) {
+                auto [p2, nscore]     = qt;
+                auto [ncurrent, ndir] = p2;
+                if (pl.get(ncurrent) == '#') continue;
                 q.push(qt);
             }
         }
     }
 
     size_t trace_back(unordered_set<set_t> target) {
-        unordered_set<pos> seen;
-        deque<set_t>       q{ target.begin(), target.end() };
-        while (q.size()) {
-            set_t key = pop(q);
+        history seen;
+        for (deque<set_t> q{ target.begin(), target.end() }; q.size(); q.pop_front()) {
+            set_t key = q.front();
 
             const auto [current, dir] = key;
             seen.insert(current);
             vector<tuple<set_t, size_t>> newkeys{ { { current - dir, dir }, 1 },
-                                                  { { current, pos{ -dir.imag(), dir.real() } }, 1000 },
-                                                  { { current, pos{ dir.imag(), -dir.real() } }, 1000 } };
+                                                  { { current, clockwise(dir) }, 1000 },
+                                                  { { current, cclockwise(dir) }, 1000 } };
             for (auto const [newkey, score] : newkeys) {
                 if (visited.contains(newkey) and visited[newkey] + score == visited[key]) q.push_back(newkey);
             }
@@ -110,7 +69,7 @@ struct maze {
 
     size_t part1() {
         size_t m = numeric_limits<size_t>::max();
-        for (visited_t vt : visited) {
+        for (auto vt : visited) {
             if (vt.first.first == end) { m = min(m, vt.second); }
         }
         return m;
@@ -119,7 +78,7 @@ struct maze {
     int part2() {
         size_t               target = part1();
         unordered_set<set_t> from;
-        for (visited_t vt : visited) {
+        for (auto vt : visited) {
             if (vt.first.first == end and vt.second == target) { from.insert(vt.first); }
         }
         return trace_back(from);
