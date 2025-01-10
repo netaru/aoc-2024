dd6095d629b522b34604eff93df761347043863b
David Jonsson <david.jonsson306@gmail.com>
2025-01-10 15:11:35 +0100

    [day09] improved the algorithm used in day09

--- 09-disk-fragmenter/main.cc
+++ 09-disk-fragmenter/main.cc
@@ -1,85 +1,91 @@
 #include <algorithm>
+#include <array>
 #include <cstddef>
-#include <cstdio>
+#include <deque>
+#include <format>
 #include <iostream>
-#include <istream>
-#include <iterator>
-#include <string>
+#include <limits>
+#include <print>
 #include <vector>
+#include "util.h"
 
-using memory_t = std::vector<int>;
+using namespace std;
 
-memory_t parse(std::istream& is) {
-    memory_t memory;
+struct memory {
+    i64 id, offset, size;
 
-    std::string s;
-    std::getline(is, s);
-    for (size_t u = 0; u < s.size(); ++u) {
-        memory_t chunk(s[u] - '0', (u + 1) % 2 ? u / 2 : -1);
-        std::copy(chunk.begin(), chunk.end(), std::back_inserter(memory));
+    memory &move(memory &other) {
+        offset = other.offset;
+        other.offset += size;
+        other.size -= size;
+        return other;
     }
-    return memory;
-}
+};
 
-bool can_possibly_defrag(memory_t& in, memory_t::reverse_iterator iter) {
-    return std::find_if(iter, in.rend(), [](const int& i) { return i == -1; }) != in.rend();
-}
+struct disk {
+    deque<memory>            occupied;
+    array<deque<memory>, 10> available;
 
-size_t chunk_size(auto iter) {
-    size_t sz    = 0;
-    int    value = (*iter);
-    while ((*iter) == value) {
-        iter++;
-        sz++;
-    }
-    return sz;
-}
-
-memory_t::reverse_iterator first_chunk(memory_t& in, memory_t::reverse_iterator iter) {
-    return std::find_if(iter, in.rend(), [](const int& i) { return i != -1; });
-}
-
-memory_t::reverse_iterator find_next_chunk(memory_t& in, memory_t::reverse_iterator iter) {
-    return std::find_if(iter, in.rend(), [&](const int& i) { return i != -1 && i != *iter; });
-}
-
-memory_t::iterator find_available(memory_t::iterator from, memory_t::iterator stop, size_t sz) {
-    while (from < stop) {
-        from             = std::find_if(from, stop, [](const int& i) { return i == -1; });
-        size_t available = chunk_size(from);
-        if (available >= sz) return from;
-        from += available;
-    }
-    return from;
-}
-
-void swap(auto iter1, auto iter2, size_t sz) {
-    for (size_t u = 0; u < sz; u++) { std::swap(*iter1++, *iter2++); }
-}
-
-template <int part = 1>
-memory_t defrag(memory_t in) {
-    for (auto iter = first_chunk(in, in.rbegin()); can_possibly_defrag(in, iter); iter = find_next_chunk(in, iter)) {
-        if constexpr (part == 1) {
-            std::swap(*std::find_if(in.begin(), in.end(), [](const int& i) { return i == -1; }), *iter);
-            continue;
+    disk(istream &is) {
+        string s = read_lines(cin).front();
+        for (size_t u = 0, offset = 0; u < s.size(); u++) {
+            size_t size = s[u] - '0';
+            if (size == 0) continue;
+            auto &curr = (u % 2 == 0) ? occupied : available[size];
+            curr.emplace_back(u / 2, offset, size);
+            offset += size;
         }
-        size_t sz   = chunk_size(iter);
-        auto   stop = in.begin() + (std::distance(iter, in.rend()) - 1), where = find_available(in.begin(), stop, sz);
-        if (where < stop) { swap(iter, where, sz); }
     }
-    return in;
-}
 
-size_t count(memory_t in) {
-    size_t sum = 0;
-    for (size_t u = 0; u < in.size(); ++u) { sum += (u * std::max(0, in[u])); }
-    return sum;
-}
+    deque<memory> fragment() {
+        deque<memory> result;
+        for (const auto m : occupied) {
+            for (size_t u = 0; u < m.size; u++) { result.emplace_back(m.id, m.offset + u, 1); }
+        }
+        return result;
+    }
 
-int main(int argc, char* argv[]) {
-    memory_t memory = parse(std::cin);
-    std::cout << "Part1: " << count(defrag(memory)) << "\n";
-    std::cout << "Part2: " << count(defrag<2>(memory)) << "\n";
+    size_t find_best(const auto &heap, size_t size, size_t coffset) {
+        size_t best = 0, offset = numeric_limits<size_t>::max();
+        for (size_t u = size; u < heap.size(); u++) {
+            if (!heap[u].size()) continue;
+            auto noffset = heap[u].front().offset;
+            if (noffset < offset and noffset < coffset) {
+                best   = u;
+                offset = noffset;
+            }
+        }
+        return best;
+    }
+
+    void push(auto &heap, memory m) {
+        if (!m.size) return;
+        auto &q = heap[m.size];
+        q.insert(lower_bound(q.begin(), q.end(), m.offset, [](auto c, auto o) { return c.offset < o; }), m);
+    }
+
+    template <int part>
+    i64 solve() {
+        deque<memory> files = occupied;
+        if constexpr (part == 1) files = fragment();
+        i64  checksum = 0;
+        auto heap     = available;
+
+        for (auto file = files.rbegin(); file != files.rend(); ++file) {
+            auto best = find_best(heap, file->size, file->offset);
+            if (best != 0) {
+                auto m = pop(heap[best]);
+                push(heap, file->move(m));
+            }
+            for (size_t u = 0; u < file->size; ++u) { checksum += (file->id * (file->offset + u)); }
+        }
+        return checksum;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    disk d(cin);
+    println("Part1: {}", d.solve<1>());
+    println("Part2: {}", d.solve<2>());
     return 0;
 }
