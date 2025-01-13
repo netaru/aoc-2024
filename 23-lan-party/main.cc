11737d7183d439ba0c0c52c89f3036c9cc0ba872
David Jonsson <david.jonsson306@gmail.com>
2025-01-13 13:19:25 +0100

    [day23] Use util on day 23 as well, set<int> -> set<string>

--- 23-lan-party/main.cc
+++ 23-lan-party/main.cc
@@ -8,26 +8,19 @@
 #include <set>
 #include <string>
 #include <unordered_map>
-#include <vector>
+#include "util.h"
 
 using namespace std;
 
-using network_t = set<int>;
-using nodes_t   = unordered_map<int, network_t>;
-
 struct LAN {
-    nodes_t nodes;
+    unordered_map<string, set<string>> nodes;
 
-    set<network_t> connected;
-
-    int    id(string s) { return s[0] | s[1] << 8; }
-    string name(int i) { return { char(i & 0xff), char(i >> 8 & 0xff) }; }
+    set<set<string>> connected;
 
     LAN(istream &is) {
-        string   s;
-        set<int> keys;
-        while (getline(is, s)) {
-            int first = id(s.substr(0, 2)), second = id(s.substr(3, 2));
+        set<string> keys;
+        for (const auto &s : read_lines(is)) {
+            string first = s.substr(0, 2), second = s.substr(3, 2);
             nodes[first].insert(second);
             nodes[second].insert(first);
             keys.insert(first);
@@ -36,34 +29,33 @@ struct LAN {
         bkb({}, keys, {});
     }
 
-    void bkb(set<int> R, set<int> P, set<int> X) {
+    void bkb(set<string> R, set<string> P, set<string> X) {
         if (P.empty() and X.empty()) {
             connected.insert(R);
         } else {
-            set<int> nn;
+            set<string> nn;
             if (!P.empty()) {
                 auto &v = nodes[*P.begin()];
-                set_difference(P.cbegin(), P.cend(), v.cbegin(), v.cend(), inserter(nn, nn.begin()));
+                ranges::set_difference(P, v, inserter(nn, nn.begin()));
             }
 
-            for (int i : nn) {
-                R.insert(i);
-                auto    &v = nodes[i];
-                set<int> newP;
-                set_intersection(P.begin(), P.end(), v.begin(), v.end(), inserter(newP, newP.begin()));
-                set<int> newX;
-                set_intersection(X.begin(), X.end(), v.begin(), v.end(), inserter(newX, newX.begin()));
-                bkb(R, newP, newX);
+            for (const string &s : nn) {
+                R.insert(s);
+                const auto &v = nodes[s];
+                set<string> newP, newX;
+                ranges::set_intersection(P, v, inserter(newP, newP.begin()));
+                ranges::set_intersection(X, v, inserter(newP, newP.begin()));
 
-                R.erase(i);
-                P.erase(i);
-                X.insert(i);
+                bkb(R, newP, newX);
+                R.erase(s);
+                P.erase(s);
+                X.insert(s);
             }
         }
     }
 
     uint64_t part1() {
-        set<set<int>> p1;
+        set<set<string>> p1;
         for (auto x : views::keys(nodes)) {
             for (auto y : nodes[x]) {
                 for (auto z : nodes[y]) {
@@ -71,34 +63,18 @@ struct LAN {
                 }
             }
         }
-        return count_if(p1.begin(), p1.end(), [&](set<int> s) {
-            return any_of(s.begin(), s.end(), [&](int i) { return name(i)[0] == 't'; });
-        });
+        return ranges::count_if(p1, [&](auto s) { return ranges::any_of(s, [&](auto c) { return c[0] == 't'; }); });
     }
 
     string part2() {
-        auto max = max_element(connected.begin(), connected.end(), [](network_t lhs, network_t rhs) {
-            return lhs.size() < rhs.size();
-        });
-
-        vector<string> vec;
-        for (auto i : *max) { vec.push_back(name(i)); }
-
-        bool   added = false;
-        string result;
-        sort(vec.begin(), vec.end());
-        for (auto s : vec) {
-            if (added) result += ",";
-            result += s;
-            added = true;
-        }
-        return result;
+        auto max = ranges::max_element(connected, [](auto lhs, auto rhs) { return lhs.size() < rhs.size(); });
+        return join(*max, ",");
     }
 };
 
 int main(int argc, char *argv[]) {
     LAN l(cin);
-    print("Part1: {}\n", l.part1());
-    print("Part2: {}\n", l.part2());
+    println("Part1: {}", l.part1());
+    println("Part2: {}", l.part2());
     return 0;
 }
