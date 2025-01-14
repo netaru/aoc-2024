226c5d2bfab30143d1c9488ba39f3679dc68db02
David Jonsson <david.jonsson306@gmail.com>
2025-01-14 18:07:36 +0100

    [day03] Using namespace std;

--- 03-mull-it-over/main.cc
+++ 03-mull-it-over/main.cc
@@ -4,22 +4,24 @@
 #include <regex>
 #include <string>
 
-int accumulator(int acc, std::smatch match) { return (std::stoi(match[1]) * std::stoi(match[2])) + acc; }
-int solve(std::string s) {
-    std::regex r("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-    return std::accumulate(std::sregex_iterator(s.begin(), s.end(), r), {}, 0, accumulator);
+using namespace std;
+
+int add(smatch m) { return (stoi(m[1]) * stoi(m[2])); }
+int solve(string s) {
+    regex r("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    return transform_reduce(sregex_iterator(s.begin(), s.end(), r), {}, 0, plus(), add);
 }
 
-std::string remove_donts(std::string s, std::string ns = "") {
+string remove_donts(string s, string ns = "") {
     ns.reserve(s.size());
-    std::regex disabled("don't\\(\\).*?(?:do\\(\\))");
-    std::regex_replace(std::back_inserter(ns), s.begin(), s.end(), disabled, "");
+    regex disabled("don't\\(\\).*?(?:do\\(\\))");
+    regex_replace(back_inserter(ns), s.begin(), s.end(), disabled, "");
     return ns;
 }
 
 int main(int argc, char *argv[]) {
-    std::string s{ std::istream_iterator<char>{ std::cin }, {} };
-    std::cout << "Part1: " << solve(s) << "\n";
-    std::cout << "Part2: " << solve(remove_donts(s)) << "\n";
+    string s{ istream_iterator<char>{ cin }, {} };
+    cout << "Part1: " << solve(s) << "\n";
+    cout << "Part2: " << solve(remove_donts(s)) << "\n";
     return 0;
 }
