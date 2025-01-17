7e196c320ae6a0dc3e56f6af5970886971d1e94d
David Jonsson <david.jonsson306@gmail.com>
2025-01-17 19:55:25 +0100

    [day22] reduce the speed of day22 by ½ still slow though

--- 22-monkey-market/main.cc
+++ 22-monkey-market/main.cc
@@ -1,72 +1,63 @@
 #include <algorithm>
 #include <cmath>
-#include <cstdint>
+#include <execution>
 #include <iostream>
 #include <iterator>
 #include <numeric>
 #include <print>
 #include <ranges>
 #include <unordered_map>
-#include <unordered_set>
-#include <utility>
 #include <vector>
 
 #include "util.h"
 
 using namespace std;
 
-using pair_t   = pair<int64_t, int64_t>;
-using banana_t = unordered_map<int64_t, int64_t>;
+using bananas_t = unordered_map<i64, i64>;
 
 struct monkey {
-    int64_t         current;
-    vector<int64_t> prices, changes;
+    i64 current, price, total, window;
 
-    monkey(int64_t seed) : current(seed) { prices.push_back(current % 10); }
+    bananas_t bananas;
 
-    constexpr int64_t mix(int64_t n, int64_t v) { return n ^ v; }
-    constexpr int64_t mul(int64_t n, int64_t v = 6) { return n << v; }
-    constexpr int64_t div(int64_t n) { return n >> 5; }
-    constexpr int64_t prune(int64_t n) { return n & 16777215; }
+    monkey(i64 seed) : current(seed), price(current % 10), total(0), window(seed + 9) {
+        for (i64 prev = price; total < 3; ++total, prev = price) {
+            price  = tick() % 10;
+            window = ((window << 5) | ((price - prev) + 9)) & 0xfffff;
+        }
+    }
 
-    int64_t tick() {
+    constexpr i64 mix(i64 n, i64 v) { return n ^ v; }
+    constexpr i64 mul(i64 n, i64 v = 6) { return n << v; }
+    constexpr i64 div(i64 n) { return n >> 5; }
+    constexpr i64 prune(i64 n) { return n & 16777215; }
+
+    i64 tick() {
         current        = prune(mix(current, mul(current)));
-        current        = prune(mix(current, div(current)));
+        current        = mix(current, div(current));
         return current = prune(mix(current, mul(current, 11)));
     }
 
-    int64_t next(int64_t n = 1) {
-        for (int64_t i = 0; i < n; ++i) {
-            prices.push_back(tick() % 10);
-            changes.push_back(prices.back() - *(prices.end() - 2));
+    i64 next(i64 n = 1) {
+        for (i64 prev = price; total < n; ++total, prev = price) {
+            price  = tick() % 10;
+            window = ((window << 5) | ((price - prev) + 9)) & 0xfffff;
+            if (!bananas.contains(window)) { bananas[window] += price; }
         }
         return current;
-    }
-
-    template <int offset = 0>
-    int64_t seq(int64_t index) const {
-        return (changes[index - offset] + 9) << (offset * 5);
-    }
-    void sell(banana_t &bananas) const {
-        unordered_set<int64_t> seen;
-        for (int i = 3; i < changes.size(); ++i) {
-            int64_t history = seq(i) | seq<1>(i) | seq<2>(i) | seq<3>(i);
-            if (!seen.contains(history)) {
-                seen.insert(history);
-                bananas[history] += prices[i + 1];
-            }
-        }
     }
 };
 
 int main(int argc, char *argv[]) {
-    banana_t bananas;
-    int64_t  part1 = accumulate(istream_iterator<int64_t>(cin), {}, 0l, [&](int64_t acc, int64_t value) {
-        monkey m(value);
-        acc += m.next(2000);
-        m.sell(bananas);
-        return acc;
-    });
+    vector<monkey> monkeys{ istream_iterator<i64>(cin), {} };
+    i64            part1 = transform_reduce(
+            execution::par, monkeys.begin(), monkeys.end(), 0l, plus(), [](monkey &m) { return m.next(2000); });
+
+    bananas_t bananas;
+    for (const auto &m : monkeys) {
+        for (const auto &[key, value] : m.bananas) bananas[key] += value;
+    }
+
     print("Part1: {}\n", part1);
     print("Part2: {}\n", rs::max(vs::values(bananas)));
     return 0;
