c54155ed94a129cb2ffa8f5253e45dac51aa3036
David Jonsson <david.jonsson306@gmail.com>
2025-01-17 10:03:20 +0100

    [day24] Reworked part1

--- 24-crossed-wires/main.cc
+++ 24-crossed-wires/main.cc
@@ -1,108 +1,60 @@
-#include <cstdint>
-#include <deque>
 #include <iostream>
-#include <optional>
+#include <map>
 #include <print>
-#include <string>
-#include <unordered_map>
+#include <ranges>
+#include <tuple>
 #include <vector>
 
 #include "util.h"
 
 using namespace std;
 
-struct gate {
-    string            which, name;
-    optional<int64_t> _left, _right, _signal;
+using bits_t = map<string, i64>;
+using wire_t = tuple<string, string, string, string>;
 
-    gate(string _which, string _name) : which(_which), name(_name) {}
+i64 read_reg(bits_t bits, char which) {
+    i64 shift = 0, ret = 0;
+    for (const auto [key, value] : bits | vs::filter([which](const auto v) { return get<0>(v)[0] == which; }))
+        ret |= value << shift++;
+    return ret;
+}
 
-    void left(int64_t sig) {
-        _left = sig;
-        eval();
-    }
-
-    void right(int64_t sig) {
-        _right = sig;
-        eval();
-    }
-
-    void eval() {
-        if (is_done()) {
-            int first = _left.value(), second = _right.value();
-            if (which == "XOR") {
-                _signal = first ^ second;
-            } else if (which == "AND") {
-                _signal = first & second;
-            } else if (which == "OR") {
-                _signal = first | second;
+i64 run(bits_t bits, vector<wire_t> wires) {
+    while (!wires.empty()) {
+        vector<wire_t> nwires;
+        for (const auto t : wires) {
+            const auto [reg1, op, reg2, out] = t;
+            if (bits.contains(reg1) and bits.contains(reg2)) {
+                if (op == "XOR") {
+                    bits[out] = bits[reg1] ^ bits[reg2];
+                } else if (op == "OR") {
+                    bits[out] = bits[reg1] | bits[reg2];
+                } else if (op == "AND") {
+                    bits[out] = bits[reg1] & bits[reg2];
+                }
+            } else {
+                nwires.push_back(t);
             }
         }
+        wires = nwires;
     }
 
-    int64_t signal() { return _signal.value(); }
-
-    bool is_done() { return (_left.has_value() and _right.has_value()); }
-};
-
-struct board {
-    deque<pair<string, int>> signals;
-
-    unordered_map<string, gate>           gates;
-    unordered_map<string, vector<string>> lefts, rights;
-
-    board(istream &is) {
-        auto input = split(read(is), "\n\n");
-        for (const string &s : split(input[0], "\n")) {
-            vector<string> line = split(s, ":");
-            signals.push_back({ line[0], stoi(line[1]) });
-        }
-        for (const string &s : split(input[1], "\n")) {
-            vector<string> line = split(s);
-            gates.insert({ line.back(), gate{ line[1], line.back() } });
-            lefts[line[0]].push_back(line.back());
-            rights[line[2]].push_back(line.back());
-        }
-    }
-
-    void light() {
-        while (!signals.empty()) {
-            const auto [name, sig] = signals.front();
-            signals.pop_front();
-
-            for (const auto l : lefts[name]) {
-                gate &g = gates.at(l);
-                g.left(sig);
-                if (g.is_done()) { signals.push_back({ l, g.signal() }); }
-            }
-            for (const auto r : rights[name]) {
-                gate &g = gates.at(r);
-                g.right(sig);
-                if (g.is_done()) { signals.push_back({ r, g.signal() }); }
-            }
-        }
-    }
-
-    int64_t part1() {
-        int64_t value = 0;
-        for (const auto [n, g] : gates) {
-            if (g.name[0] == 'z') { value |= (g._signal.value() << stol(g.name.substr(1))); }
-        }
-        return value;
-    }
-
-    void out() {
-        for (const auto [n, g] : gates) {
-            cout << "Gate: " << g.name << " has gate " << g.which << ", left: " << g._left.value_or(-1)
-                 << ", right: " << g._right.value_or(-1) << ", signal: " << g._signal.value_or(-1) << "\n";
-        }
-    }
-};
+    return read_reg(bits, 'z');
+}
 
 int main(int argc, char *argv[]) {
-    board b(cin);
-    b.light();
-    // b.out();
-    print("Part1: {}\n", b.part1());
+    auto           input = split(read(cin), "\n\n");
+    bits_t         bits;
+    vector<wire_t> wires;
+    for (const string &s : split(input[0], "\n")) {
+        auto row     = split(s, ": ");
+        bits[row[0]] = stoi(row[1]);
+    }
+    for (const string &s : split(input[1], "\n")) {
+        auto row = split(s);
+        wires.emplace_back(row[0], row[1], row[2], row[4]);
+    }
+    println("Part1: {}", run(bits, wires));
+    println("But it should be: {}", read_reg(bits, 'x') + read_reg(bits, 'y'));
     return 0;
 }
