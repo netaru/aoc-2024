c3e91816b453e4e498fc73a5c69f016978e81d20
David Jonsson <david.jonsson306@gmail.com>
2025-01-19 22:04:06 +0100

    [day19] Use ranges instead

--- 19-linen-layout/main.cc
+++ 19-linen-layout/main.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <numeric>
 #include <print>
@@ -12,32 +13,27 @@ using namespace std;
 
 using cache_t = unordered_map<string_view, u64>;
 struct linen {
-    vector<string> towels, patterns;
+    vector<string> input, towels, patterns;
     cache_t        cache;
 
-    linen(istream &is) {
-        auto input = split(read(is), "\n\n");
-        towels     = split(input[0], ",");
-        patterns   = split(input[1], "\n");
-    }
+    linen(istream &is)
+        : input(split(read(is), "\n\n")), towels(split(input[0], ",")), patterns(split(input[1], "\n")) {}
 
     u64 can_make(string_view pattern) {
-        if (!pattern.size()) return 1;
+        if (pattern.empty()) { return 1; }
         if (cache.contains(pattern)) { return cache[pattern]; }
         u64 solutions = 0;
-        for (const string towel : towels | vs::filter([&](string s) { return pattern.substr(0, s.size()) == s; })) {
+        for (const string towel : towels | vs::filter([&](string s) { return pattern.substr(0, s.size()) == s; }))
             solutions += can_make(pattern.substr(towel.size()));
-        }
         return cache[pattern] = solutions;
     }
 
     u64 part1() {
-        return transform_reduce(
-                patterns.begin(), patterns.end(), 0ul, plus(), [&](string c) { return can_make(c) ? 1 : 0; });
+        return rs::fold_left(patterns | vs::transform([&](auto p) { return can_make(p) ? 1 : 0; }), 0ul, plus());
     }
 
     u64 part2() {
-        return transform_reduce(patterns.begin(), patterns.end(), 0ul, plus(), [&](string c) { return can_make(c); });
+        return rs::fold_left(patterns | vs::transform([&](auto p) { return can_make(p); }), 0ul, plus());
     }
 };
 
