7f2b409f67ff912842e0e0795cde5307eceeb4c8
David Jonsson <david.jonsson306@gmail.com>
2025-01-22 21:52:32 +0100

    [day02] Day02 improvements

--- 02-red-nosed-reports/main.cc
+++ 02-red-nosed-reports/main.cc
@@ -1,10 +1,4 @@
-#include <algorithm>
-#include <cstdlib>
-#include <functional>
 #include <iostream>
-#include <ranges>
-#include <tuple>
-#include <vector>
 
 #include "util.h"
 
@@ -13,24 +7,25 @@ using namespace std;
 using report = vector<int>;
 using reports = vector<report>;
 
-bool is_sorted(report r) { return is_sorted(r.begin(), r.end()) or is_sorted(r.begin(), r.end(), greater<int>()); }
+bool is_sorted(span<int> r) { return rs::is_sorted(r) or rs::is_sorted(r | vs::reverse); }
 
-bool element_difference_check(report r) {
+bool element_difference_check(span<int> r) {
     auto view = r | vs::adjacent<2>;
-    auto it = rs::find_if(view, [](tuple<int, int> p) {
-        int value = abs(get<0>(p) - get<1>(p));
+    auto it = rs::find_if(view, [](auto t) {
+        const auto [first, second] = t;
+        int value = abs(first - second);
         return (value > 3 or value == 0);
     });
     return it == view.end();
 }
 
-bool part1_predicate(report r) { return is_sorted(r) and element_difference_check(r); }
+bool part1_predicate(span<int> r) { return is_sorted(r) and element_difference_check(r); }
 
-bool part2_predicate(report r) {
+bool part2_predicate(span<int> r) {
     bool safe = part1_predicate(r);
     if (!safe) {
         for (int i = 0; i < r.size(); i++) {
-            report tmp = r;
+            report tmp{ r.begin(), r.end() };
             tmp.erase(tmp.begin() + i);
             if (safe = part1_predicate(tmp); safe) break;
         }
@@ -38,15 +33,9 @@ bool part2_predicate(report r) {
     return safe;
 }
 
-reports parse() {
-    reports rs;
-    for (const auto s : read_lines(cin)) rs.push_back(split<int>(s));
-    return rs;
-}
-
 int main(int argc, char *argv[]) {
-    reports rs = parse();
-    cout << "Part1: " << rs::count_if(rs, part1_predicate) << "\n";
-    cout << "Part2: " << rs::count_if(rs, part2_predicate) << "\n";
+    reports rs = read_lines(cin) | vs::transform([](auto s) { return split<int>(s); }) | rs::to<reports>();
+    println("Part1: {}", rs::count_if(rs, part1_predicate));
+    println("Part2: {}", rs::count_if(rs, part2_predicate));
     return 0;
 }
