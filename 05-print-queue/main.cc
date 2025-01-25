9ee1b47fc4396fbd01c892b961654e8e42f7ac46
David Jonsson <david.jonsson306@gmail.com>
2025-01-25 21:13:10 +0100

    [day05] Use ranges library to build vector

--- 05-print-queue/main.cc
+++ 05-print-queue/main.cc
@@ -8,16 +8,16 @@
 #include "util.h"
 
 using namespace std;
+using pages_t = vector<vector<int>>;
 constexpr int middle(const auto &p) { return p[(p.size() / 2)]; }
 
 int main(int argc, char *argv[]) {
     map<int, set<int>> rules;
-    vector<vector<int>> pages;
     auto comp{ [&rules](int lhs, int rhs) { return rules[lhs].contains(rhs); } };
-    auto input = split(read(cin), "\n\n");
-    for (const auto &rule : split(input[0], "\n") | vs::transform([](auto s) { return split<int>(s, "|"); }))
+    auto in = split(read(cin), "\n\n");
+    for (const auto &rule : split(in[0], "\n") | vs::transform([](auto s) { return split<int>(s, "|"); }))
         rules[rule.front()].insert(rule.back());
-    for (const auto &s : split(input[1], "\n")) { pages.push_back(split<int>(s, ",")); }
+    pages_t pages = split(in[1], "\n") | vs::transform([](auto s) { return split<int>(s, ","); }) | rs::to<pages_t>();
     auto fn1 = [&](auto p) { return rs::is_sorted(p, comp) ? middle(p) : 0; };
     auto fn2 = [&](auto p) { return rs::is_sorted(p, comp) ? 0 : middle(dave::sort(p, comp)); };
     println("Part1: {}", rs::fold_left(pages | vs::transform(fn1), 0, plus()));
