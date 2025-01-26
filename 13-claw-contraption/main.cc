38905565a977ea1270a6fe228634008e1707b1c6
David Jonsson <david.jonsson306@gmail.com>
2025-01-26 14:18:45 +0100

    [day13] Make the view into a vector

--- 13-claw-contraption/main.cc
+++ 13-claw-contraption/main.cc
@@ -1,16 +1,12 @@
 #include <cmath>
-#include <cstdint>
 #include <iostream>
-#include <numeric>
-#include <print>
-#include <ranges>
 
 #include "util.h"
 
 using namespace std;
 
 template <int part>
-int64_t score(double ax, double ay, double bx, double by, double xtarget, double ytarget) {
+i64 score(double ax, double ay, double bx, double by, double xtarget, double ytarget) {
     if constexpr (part == 2) {
         xtarget += 10000000000000;
         ytarget += 10000000000000;
@@ -21,14 +17,13 @@ int64_t score(double ax, double ay, double bx, double by, double xtarget, double
     return 0;
 }
 
-int64_t solve(const auto &c, auto func) {
-    return transform_reduce(c.cbegin(), c.cend(), 0l, std::plus<int64_t>(), [&](auto v) {
-        return func(v[0], v[1], v[2], v[3], v[4], v[5]);
-    });
+i64 solve(const auto &c, auto func) {
+    auto fn = [&](auto v) { return func(v[0], v[1], v[2], v[3], v[4], v[5]); };
+    return rs::fold_left(c | vs::transform(fn), 0l, plus());
 }
 
 int main(int argc, char *argv[]) {
-    auto chunks = ints<double>(read(cin)) | vs::chunk(6);
+    auto chunks = ints<double>(read(cin)) | vs::chunk(6) | rs::to<vector>();
     println("Part1: {}", solve(chunks, score<1>));
     println("Part2: {}", solve(chunks, score<2>));
     return 0;
