f0ba701e72aeeff06fbc1920466ab3cef4986d00
David Jonsson <david.jonsson306@gmail.com>
2025-01-26 14:58:21 +0100

    [day9] part2 added

--- 09-disk-framenter/main.cc
+++ 09-disk-framenter/main.cc
@@ -7,83 +7,79 @@
 #include <string>
 #include <vector>
 
-struct chunk {
-    int  id;
-    bool is_file() const { return id != -1; }
-};
-
-using memory_t = std::vector<chunk>;
-
-memory_t create_chunks(size_t sz, int id) {
-    chunk    c{ .id = id };
-    memory_t result(sz, c);
-    return result;
-}
+using memory_t = std::vector<int>;
 
 memory_t parse(std::istream& is) {
+    memory_t memory;
+
     std::string s;
-    memory_t    result;
-    int         id = 0;
     std::getline(is, s);
-    bool is_file = true;
-    for (const char c : s) {
-        if (is_file) {
-            memory_t chunks = create_chunks(c - '0', id++);
-            std::copy(chunks.begin(), chunks.end(), std::back_inserter(result));
-        } else {
-            memory_t chunks = create_chunks(c - '0', -1);
-            std::copy(chunks.begin(), chunks.end(), std::back_inserter(result));
-        }
-        is_file = !is_file;
+    for (size_t u = 0; u < s.size(); ++u) {
+        memory_t chunk(s[u] - '0', (u + 1) % 2 ? u / 2 : -1);
+        std::copy(chunk.begin(), chunk.end(), std::back_inserter(memory));
     }
-    return result;
+    return memory;
 }
 
-void print(memory_t m) {
-    for (auto block : m) {
-        if (block.is_file())
-            std::cout << block.id;
-        else
-            std::cout << '.';
+bool can_possibly_defrag(memory_t& in, memory_t::reverse_iterator iter) {
+    return std::find_if(iter, in.rend(), [](const int& i) { return i == -1; }) != in.rend();
+}
+
+size_t chunk_size(auto iter) {
+    size_t sz    = 0;
+    int    value = (*iter);
+    while ((*iter) == value) {
+        iter++;
+        sz++;
     }
-    std::cout << "\n";
+    return sz;
 }
 
-void swap(auto first, auto second) {
-    chunk tmp = *first;
-    *first    = *second;
-    *second   = tmp;
+memory_t::reverse_iterator first_chunk(memory_t& in, memory_t::reverse_iterator iter) {
+    return std::find_if(iter, in.rend(), [](const int& i) { return i != -1; });
 }
 
-memory_t::reverse_iterator find_next(memory_t& in, memory_t::reverse_iterator iter) {
-    return std::find_if(iter, in.rend(), [](const chunk& ch) { return ch.is_file(); });
+memory_t::reverse_iterator find_next_chunk(memory_t& in, memory_t::reverse_iterator iter) {
+    return std::find_if(iter, in.rend(), [&](const int& i) { return i != -1 && i != *iter; });
 }
 
-bool can_defrag(memory_t& in, memory_t::reverse_iterator iter) {
-    return std::find_if(iter, in.rend(), [](const chunk& ch) { return !ch.is_file(); }) != in.rend();
+memory_t::iterator find_available(memory_t::iterator from, memory_t::iterator stop, size_t sz) {
+    while (from < stop) {
+        from             = std::find_if(from, stop, [](const int& i) { return i == -1; });
+        size_t available = chunk_size(from);
+        if (available >= sz) return from;
+        from += available;
+    }
+    return from;
 }
 
+void swap(auto iter1, auto iter2, size_t sz) {
+    for (size_t u = 0; u < sz; u++) { std::swap(*iter1++, *iter2++); }
+}
+
+template <int part = 1>
 memory_t defrag(memory_t in) {
-    for (memory_t::reverse_iterator iter = find_next(in, in.rbegin()); can_defrag(in, iter);
-         iter                            = find_next(in, iter)) {
-        swap(std::find_if(in.begin(), in.end(), [](const chunk& ch) { return !ch.is_file(); }), iter);
+    for (auto iter = first_chunk(in, in.rbegin()); can_possibly_defrag(in, iter); iter = find_next_chunk(in, iter)) {
+        if constexpr (part == 1) {
+            std::swap(*std::find_if(in.begin(), in.end(), [](const int& i) { return i == -1; }), *iter);
+            continue;
+        }
+        size_t sz   = chunk_size(iter);
+        auto   stop = in.begin() + (std::distance(iter, in.rend()) - 1), where = find_available(in.begin(), stop, sz);
+        if (where < stop) { swap(iter, where, sz); }
     }
     return in;
 }
 
 size_t count(memory_t in) {
     size_t sum = 0;
-    for (size_t u = 0; in[u].is_file(); ++u) {
-        sum += (u * in[u].id);
-    }
+    for (size_t u = 0; u < in.size(); ++u) { sum += (u * std::max(0, in[u])); }
     return sum;
 }
 
 int main(int argc, char* argv[]) {
-    auto memory = parse(std::cin);
-    // print(memory);
-    memory = defrag(memory);
-    // print(memory);
-    std::cout << "Part1: " << count(memory) << "\n";
+    memory_t memory = parse(std::cin);
+    std::cout << "Part1: " << count(defrag(memory)) << "\n";
+    std::cout << "Part2: " << count(defrag<2>(memory)) << "\n";
     return 0;
 }
