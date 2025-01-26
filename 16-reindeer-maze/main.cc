ac49c8bcdb9d62edeeda90882006e6e006965c3f
David Jonsson <david.jonsson306@gmail.com>
2025-01-26 15:02:07 +0100

    [day16] Added slow part2

--- 16-reindeer-maze/main.cc
+++ 16-reindeer-maze/main.cc
@@ -1,10 +1,13 @@
+#include <algorithm>
 #include <complex>
 #include <deque>
 #include <iostream>
+#include <iterator>
 #include <print>
 #include <string>
 #include <tuple>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -25,7 +28,7 @@ auto pop(auto &q) {
     return p;
 }
 
-using step = tuple<pos, int, int>;
+using step = tuple<pos, int, int, unordered_set<pos>>;
 
 struct maze {
     vector<string> grid;
@@ -63,25 +66,65 @@ struct maze {
     int part1() {
         unordered_map<pos, int> visited;
 
-        deque<step> paths{ { start, 0, 0 } };
+        deque<step> paths{ { start, 0, 0, {} } };
         while (paths.size()) {
-            auto [current, dir, score] = pop(paths);
+            auto [current, dir, score, history] = pop(paths);
             if (get(current) == '#') continue;
             if (visited.contains(current) and visited[current] <= score) { continue; }
             visited[current] = score;
 
             int left = turn_left(dir), right = turn_right(dir);
-            paths.push_back({ current + dirs[left], left, score + 1001 });
-            paths.push_back({ current + dirs[dir], dir, score + 1 });
-            paths.push_back({ current + dirs[right], right, score + 1001 });
+            paths.push_back({ current + dirs[left], left, score + 1001, history });
+            paths.push_back({ current + dirs[dir], dir, score + 1, history });
+            paths.push_back({ current + dirs[right], right, score + 1001, history });
         }
 
         return visited[end];
+    }
+
+    // 7036
+    // 11048
+    // 101492
+    int part2() {
+        unordered_map<pos, int> visited;
+        unordered_set<pos>      best;
+
+        deque<step> paths{ { start, 0, 0, {} } };
+        while (paths.size()) {
+            auto [current, dir, score, history] = pop(paths);
+            if (get(current) == '#') continue;
+            if (visited.contains(current) and visited[current] * 2 < score) { continue; }
+            if (!visited.contains(current)) {
+                visited[current] = score;
+            } else if (score < visited[current]) {
+                visited[current] = score;
+            }
+            history.insert(current);
+            if (current == end and score == 101492) {
+                copy(history.begin(), history.end(), inserter(best, best.begin()));
+            }
+
+            int left = turn_left(dir), right = turn_right(dir);
+            paths.push_back({ current + dirs[left], left, score + 1001, history });
+            paths.push_back({ current + dirs[dir], dir, score + 1, history });
+            paths.push_back({ current + dirs[right], right, score + 1001, history });
+        }
+
+        print_grid(best);
+
+        return best.size();
+    }
+
+    void print_grid(unordered_set<pos> paths) {
+        auto cp = grid;
+        for (auto p : paths) { cp[p.imag()][p.real()] = 'O'; }
+        copy(cp.begin(), cp.end(), ostream_iterator<string>(cout, "\n"));
     }
 };
 
 int main(int argc, char *argv[]) {
     maze m(cin);
     print("Part1: {}\n", m.part1());
+    print("Part2: {}\n", m.part2());
     return 0;
 }
